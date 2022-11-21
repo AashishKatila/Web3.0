695477f17ee7ae7907076a7e305f380f7eb40240
AashishKatila <aashishkatila@gmail.com>
2022-11-21 11:19:32 +0545

    third commit: completed service section

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,37 @@
+import react,{useContext} from 'react';
+import {TransactionContext} from '../context/TransactionContext'
+import  dummyData from '../utils/dummyData'
+import {shortenAddress} from '../utils/shortenAddress';
+const TransactionCard = ({addressTo,addressFrom,timestamp,message,keyword,amount,url}) => {
+    <div className= 'bg -[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl '>
+        <div className="flex flex-col items-center w-full mt-3">
+            <div className="display-flex justify-start w-full mb-6 p-2">
+                <a ></a> 
+            </div>
+        </div>
+    </div>
+}
+
 const Transactions = () =>{
+    const { currentAccount } = useContext(TransactionContext)
+
     return (
-        <h1>Transactions</h1>
+        <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+                {currentAccount ?(
+                    <h3 className='text-white text-3xl text-center my-2'>Latest Transactions</h3>
+                ):
+                (
+                    <h3 className='text-white text-3xl text-center my-2'>Connect your account to see latest transaction</h3>
+                )
+                }
+                <div className="flex flex-wrap justify-center items-center mt-10">
+                    {[dummyData.reverse().map((transaction,i) =>(
+                        <TransactionCard key={i} {...transaction} />
+                    ))]}
+                </div>
+            </div>
+        </div>
     )
 }
 
